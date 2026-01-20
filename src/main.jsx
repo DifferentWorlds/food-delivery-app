82bb01a066cd2d0baba698c18dfca08ef44d35f6
shivani <shivaniyenkerala145@gmail.com>
2026-01-20 18:27:17 +1300

    final changes

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-    <HashRouter basename='/food-delivery-app'>
+    <HashRouter>
   <App />
 </HashRouter>
 )
