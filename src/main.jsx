16d19fb57f1db9d6eb63271110f3a1bea6bf0497
shivani <shivaniyenkerala145@gmail.com>
2026-01-20 18:11:03 +1300

    add basename

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-    <HashRouter>
+    <HashRouter basename='/food-delivery-app'>
   <App />
 </HashRouter>
 )
