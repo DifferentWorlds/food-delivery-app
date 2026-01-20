fa764990157c225d93d4d49d8f2522bffca8d2ae
shivani <shivaniyenkerala145@gmail.com>
2026-01-20 13:47:26 +1300

    update router seetings

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-
 import './index.css'
-
-
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <HashRouter>
+  <App />
+</HashRouter>
 )
